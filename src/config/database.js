42807c167e722c7b6b7eeeca71907ed4ce1618ba
Lucas Ayrton <lucas.soares@cassems.com.br>
2018-11-30 14:40:51 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '123',
+  database: 'gonodemodule2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
